f145715b440064434648ad55d3686ac2eb3556d0
Arnaud Lalouette <arnaud.lalouette@its4u.com>
2020-06-09 11:25:39 +0200

    first commit

---
+++ src/test/java/lu/its4u/mysaam/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package lu.its4u.mysaam;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
